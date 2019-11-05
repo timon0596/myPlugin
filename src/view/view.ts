02ff8dc6a6457fd7f94e3e050b2efe455f220b97
timon <timon0596@yandex.ru>
2019-11-05 04:49:12 +0300

    range

--- src/view/view.ts
+++ src/view/view.ts
@@ -26,37 +26,42 @@
 		$(...title).appendTo('.handle')
 
 		let handleX: Array<number> = [0,0]
-
-		let deltaX: Array<number> = [0,0]
+		let handlesMinMax: Array<number> = [0,0]
 
 		let handles: any = $('.handle')
-
 		$(handles).each((i,el)=>{
 			$(el).mousedown((e)=>{
-				handleX[i]=e.clientX
 				$('body').mousemove((ev)=>{
-					deltaX[i]=ev.clientX-handleX[i]
-					handleX[i]=ev.clientX
-					console.log($(el).parent()[0].getBoundingClientRect())
+
 					$(el).css({left: ()=>{
-							if(ev.clientX<=parseFloat($(el).parent()[0].getBoundingClientRect().left)){
+							if(ev.clientX<=$(el).parent()[0].getBoundingClientRect().left){
 								return 0
 							}
-							if(ev.clientX>=parseFloat($(el).parent()[0].getBoundingClientRect().left+$(el).parent()[0].getBoundingClientRect().width)-parseFloat($(el).css('width'))){
-								return parseFloat($(el).parent()[0].getBoundingClientRect().width)-parseFloat($(el).css('width'))
+							if(ev.clientX>=$(el).parent()[0].getBoundingClientRect().left+$(el).parent()[0].getBoundingClientRect().width-parseFloat($(el).css('width'))){
+								return $(el).parent()[0].getBoundingClientRect().width-parseFloat($(el).css('width'))
 							}else{
-								return ev.clientX-parseFloat($(el).parent()[0].getBoundingClientRect().left)
+								return ev.clientX-$(el).parent()[0].getBoundingClientRect().left
 							}
 						}
 					})
+					handleX[i]=parseFloat($(el).css('left'))
+					if(handleX[0]>handleX[1]){
+						[handlesMinMax[0],handlesMinMax[1]]=[handleX[1],handleX[0]]
+					}else{
+						[handlesMinMax[0],handlesMinMax[1]]=[handleX[0],handleX[1]]
+					}
+					$('.range').css({
+						left: ()=>handlesMinMax[0],
+						width: ()=>handlesMinMax[1]-handlesMinMax[0]+parseFloat($(el).css('width'))
+					})
+					console.log($('.range').css(['left','width']))
 				})
 			})
-			$('body').mouseup(function(){
-
-				$('body').unbind('mousemove')
-			})
 		})
-		
+		$('body').mouseup(function(){
+				$('body').unbind('mousemove')
+				
+			})
 		return this;
 	}
 }(jQuery))
