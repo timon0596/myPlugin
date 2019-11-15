ff27d7dfc7150601fbc1edeafb42e5bc48d3e44b
timon <timon0596@yandex.ru>
2019-11-16 02:37:29 +0300

    vertical handle movement is tuned

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -14,8 +14,12 @@ class ViewClass {
 	params: any = {
 		interval: (s: boolean) : void => {
 			if(s){
+				this.baseSlider.append(this.handle)
+			}
+		},
+		range: (s: boolean) : void => {
+			if(s){
 				this.baseSlider.append(this.range)
-				console.log(this.baseSlider)
 			}
 		},
 		vertical: (s: boolean) : void => {
@@ -40,6 +44,24 @@ class ViewClass {
 
 	
 }
+
+class ModelClass {
+	constructor(public settings: any, public parent: any){
+
+	}
+	
+	handleBot({handle, slider, mousemoveY, fromBoundDistance,handleBottom}: any): any {
+		if(mousemoveY>slider.top+slider.height-fromBoundDistance){
+			return 0
+		}
+		
+		if(mousemoveY<slider.top+handle.height-fromBoundDistance){
+			return slider.height-handle.height
+		}
+		return slider.height+Number((slider.top).toFixed(1))-Number((mousemoveY).toFixed(1)) - Number((fromBoundDistance).toFixed(1))
+		
+	}
+}
 (function($){
 	$.fn.timonSliderPlugin = function(options?: any){
 		let $this = this
@@ -48,13 +70,43 @@ class ViewClass {
 				{
 					vertical: true,
 					title: true,
+					range: true,
 					interval: true
 				},
 			step: 1
 			
 		        }, options );
 		let view = new ViewClass(settings, this)
+		let model = new ModelClass(settings, this)
+		let obj: any = {
+			}
+			let click: boolean
 		view.render()
+		$('body').mousedown((e)=>{
+			if(e.target == this.find('.slider .handle')[0]||e.target == this.find('.slider .handle')[1]){
+				obj.handle = e.target.getBoundingClientRect()
+				obj.target = $(e.target)[0]
+				obj.handleBottom = parseFloat(obj.target.style.bottom)
+				obj.slider = $(e.target).parent()[0].getBoundingClientRect()
+				obj.fromBoundDistance = e.target.getBoundingClientRect().top + e.target.getBoundingClientRect().height - e.clientY
+				
+				click=true
+			}
+
+		})
+		$('body').mousemove((e)=>{
+			if(click){
+				
+				obj.mousemoveY = e.clientY
+				obj.handleBottom = parseFloat(obj.target.style.bottom)
+				console.log(obj.handleBottom)
+				$(obj.target).css({bottom: model.handleBot(obj)})
+
+			}
+		})
+		$('body').mouseup(()=>{
+			click=false
+		})
 		return this;
 	}
 }(jQuery))
