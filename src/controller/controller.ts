77a1878b410e03c2d12f870c8dec70699c828821
timon <timon0596@yandex.ru>
2020-05-31 06:42:57 +0300

    handle is moving by steps

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,12 +1,17 @@
 class slider{
-	vertical:boolean = false;
-	handlesNumber:number = 1;
-	handles:Array<any> = [new handle(0)];
-	range:Array<number|string> =  [0,100];
-	initialValues:Array<number|string> =  [0];
-	section:boolean= false;
-	step:number = 30;
+	vertical:boolean = true
+	handlesNumber:number = 1
+	handles:Array<any> = [new handle(this.vertical)]
+	range:Array<number|string> =  [0,100]
+	initialValues:Array<number|string> =  [0]
+	section:boolean= false
+	step:number = 30
 	el:HTMLElement
+	stepSize:number = 0
+	private mouseX: number = 0
+	private mouseY: number = 0
+	private mousePos: number = 0
+
 	constructor(){
 		this.el = document.createElement('div')
 		this.el.classList.add('slider')
@@ -17,25 +22,45 @@ class slider{
 		$(innerDiv).append(this.handles[0].el)
 		$(this.el).append(innerDiv)
 	}
+
+
+	countMousePos(e:MouseEvent){
+			this.mousePos = this.vertical?
+				(this.el.offsetTop + this.el.offsetHeight - e.clientY):
+				(e.clientX - this.el.offsetLeft)
+			if(this.mousePos<0){
+				this.mousePos = 0
+			}
+			else if(this.mousePos>this.el[this.vertical?'offsetHeight':'offsetWidth']){
+				this.mousePos=this.el[this.vertical?'offsetHeight':'offsetWidth']
+				if(Math.round(this.mousePos/this.stepSize)*this.stepSize<this.el[this.vertical?'offsetHeight':'offsetWidth']){
+					return this.el[this.vertical?'offsetHeight':'offsetWidth']
+				}
+			}
+			return Math.round(this.mousePos/this.stepSize)*this.stepSize
+	}
+
 };
+
 class handle{
 	hasTitle:boolean = true
 	private titleValue:string|number = 0
-	private htmlString:HTMLElement
-	constructor(public x:number){
-		this.htmlString = document.createElement('div')
-		this.htmlString.classList.add('handle')
-		this.htmlString.innerHTML = this.hasTitle?
+	private el:HTMLElement
+	constructor(private vertical:boolean){
+		
+		this.el = document.createElement('div')
+		this.el.classList.add('handle')
+		this.el.innerHTML = this.hasTitle?
 			`<div class="title__wrapper"><div class="title">${this.titleValue}</div></div>`:''
 	}
 
 	set title(title:string|number){
 		this.titleValue = title
-		this.htmlString.innerHTML = this.hasTitle?
+		this.el.innerHTML = this.hasTitle?
 			`<div class="title__wrapper"><div class="title">${this.titleValue}</div></div>`:''
 	}
-	get el(){
-		return this.htmlString
+	setPosition(positionValue: number){
+		this.el.style[this.vertical?'bottom':'left'] =  positionValue + 'px'
 	}
 };
 let title: any = {
@@ -44,20 +69,22 @@ let title: any = {
 
 class controller{
 
-	private mouseX: number = 0
-	private mouseY: number = 0
+	
 	private mousedown: boolean = false;
-	private handle: HTMLElement = this.slider.handles[0];
+	private handle: any = this.slider.handles[0];
 	constructor(private slider: any){
-
+		let $this = this 
 
 		$('.slider1.test').append(this.slider.el)
 
-		$(this.slider.handles[0].el).on('mousedown',(e)=>{
-			this.mousedown=true
-			this.handle=e.currentTarget
-			console.log(this.handle)
-			
+
+		this.slider.stepSize = this.slider.el[this.slider.vertical?'offsetHeight':'offsetWidth']/(Math.abs(this.slider.range[0] - this.slider.range[1])/this.slider.step)
+		
+		$(this.slider.handles).each(function(i,el){
+			$(el.el).on('mousedown',(e)=>{
+				$this.mousedown=true
+				$this.handle = this
+			})
 		})
 
 		$(document).on('mouseup',()=>{
@@ -65,31 +92,7 @@ class controller{
 		})
 
 		$(document).on('mousemove',(e)=>{
-			if(this.mousedown)
-				{
-					if(this.slider.vertical){
-						this.mouseY = this.slider.el.offsetTop + this.slider.el.offsetHeight - e.clientY
-						if(this.mouseY<0){
-							this.mouseY = 0
-						}
-						else if(this.mouseY>this.slider.el.offsetHeight ){
-							this.mouseY=this.slider.el.offsetHeight
-						}
-						this.handle.style.bottom = Math.round(this.mouseY/this.slider.step)*this.slider.step + 'px'
-					}else{
-						this.mouseX = e.clientX - this.slider.el.offsetLeft
-						if(this.mouseX<0){
-							this.mouseX = 0
-						}
-						if(this.mouseX>this.slider.el.offsetWidth ){
-							this.mouseX=this.slider.el.offsetWidth
-						}
-						this.handle.style.left = Math.round(this.mouseX/this.slider.step)*this.slider.step + 'px'
-						
-					}
-					
-				}
-				
+			this.mousedown ? this.handle.setPosition(this.slider.countMousePos(e)) : null
 		})
 		
 	}
