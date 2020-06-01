8b7d0a613e5194169b3857632b51dfd216c429a4
timon <timon0596@yandex.ru>
2020-06-02 00:10:57 +0300

    initial values + title values

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,20 +1,20 @@
 class slider{
-	vertical:boolean = true
+	vertical:boolean = false
 	range:boolean = true
 	rangeElement:any = this.range?new RangeSection(this.vertical):null
 	minmax: [number,number]|null = this.range?[0,0]:null
-	handlesNumber:number = 2
+	handlesNumber:number = 3
 	handles:Array<any> = []
-	diapason:Array<string>|[number,number] =  ['0,100','0,100','0,100','0,100','0,100','0,100','0,100','0,100','0,100']
-	initialValues:Array<number|string> =  [0]
+	diapason:Array<string>|[number,number] =  ["Москва","Московская обл.","Санкт-Петербург","Ленинградская обл.","Абаза","Респ. Хакасия"]
+	initialValues:Array<number|string> =  [1]
 	section:boolean= false
-	step:number = 1
+	step:number = 4
 	el:HTMLElement
 	stepSize:number = 0
 	private mousePos: number = 0
 
 	constructor(){
-		
+		typeof this.initialValues[0]!=typeof this.diapason[0]?this.initialValues[0]=this.diapason[0]:null
 		this.el = document.createElement('div')
 		this.el.classList.add('slider')
 		this.vertical ? this.el.classList.add('vertical'):null
@@ -34,6 +34,7 @@ class slider{
 				(this.el[this.vertical?'offsetHeight':'offsetWidth']/(this.diapason.length-1))*this.step:
 				(this.el[this.vertical?'offsetHeight':'offsetWidth']/Math.abs(<number>this.diapason[0]-<number>this.diapason[1])*this.step)
 		})
+
 	}
 
 
@@ -55,6 +56,52 @@ class slider{
 	minmaxCompute(){
 		this.minmax?
 			(this.minmax[0]=Math.min(...this.handles.map(el=>el.pos)),this.minmax[1]=Math.max(...this.handles.map(el=>el.pos))) : null
+	}
+	computeTitleValue(e:MouseEvent|number){
+		if(typeof e!='number'){
+			if(typeof this.diapason[0]=='number')
+				return Math.trunc(this.countMousePos(e)/this.el[this.vertical?'offsetHeight':'offsetWidth']*Math.abs(<number>this.diapason[1]-<number>this.diapason[0]))+this.diapason[0]
+			else
+				{
+					console.log(this.countMousePos(e))
+					return this.diapason[this.countMousePos(e)/(this.el[this.vertical?'offsetHeight':'offsetWidth']/(this.diapason.length-1))]
+				}
+		}
+		else{
+			if(typeof this.diapason[0]=='number')
+				return Math.trunc(e/this.el[this.vertical?'offsetHeight':'offsetWidth']*Math.abs(<number>this.diapason[1]-<number>this.diapason[0]))+this.diapason[0]
+			else
+				return this.diapason[e/this.stepSize]
+		}
+	}
+	computeInitialPositions(values: Array<string>|Array<number>){
+		if(typeof values[0]=='string'){
+			$(values).each((i,el)=>{
+				let ind=this.diapason.indexOf(<never>el)
+				ind!=-1?
+					this.handles[i].setPosition(ind*this.stepSize):
+					this.handles[i].setPosition(0)
+			})
+		}
+		if(typeof values[0]=='number'){
+			$(values).each((i,el)=>{
+				<never>el<this.diapason[0]?
+					this.handles[i].setPosition(0):
+					<never>el>this.diapason[1]?
+						this.handles[i].setPosition(this.el[this.vertical?'offsetHeight':'offsetWidth']):
+						this.handles[i].setPosition(
+							Math.round((<never>el-<number>this.diapason[0])/
+								Math.abs(<number>this.diapason[1]-<number>this.diapason[0])*
+								this.el[this.vertical?'offsetHeight':'offsetWidth']/
+								this.stepSize)*
+							this.stepSize
+						)
+						console.log(this.stepSize)
+
+
+			})
+
+		}
 	}
 
 
@@ -78,6 +125,7 @@ class handle{
 		
 		this.el = document.createElement('div')
 		this.el.classList.add('handle')
+		this.el.style[this.vertical?'bottom':'left'] =  this.pos + 'px'
 		this.el.innerHTML = this.hasTitle?
 			`<div class="title__wrapper"><div class="title">${this.titleValue}</div></div>`:''
 	}
@@ -91,10 +139,9 @@ class handle{
 		this.el.style[this.vertical?'bottom':'left'] =  positionValue + 'px'
 		this.pos=positionValue
 	}
+
 };
-let title: any = {
-	value: ''
-};
+
 
 class controller{
 
@@ -106,6 +153,14 @@ class controller{
 
 		$('.slider1.test').append(this.slider.el)
 
+		$(document).ready(()=>{
+			this.slider.computeInitialPositions(this.slider.initialValues)
+			$(this.slider.handles).each((i,el)=>{
+				el.title = this.slider.computeTitleValue(el.pos)
+			})
+			this.slider.minmaxCompute(),
+			this.slider.rangeElement?this.slider.rangeElement.render(...this.slider.minmax):null
+		})
 
 		
 		
@@ -118,7 +173,6 @@ class controller{
 
 		$(document).on('mouseup',()=>{
 			this.mousedown=false
-			console.dir(this.slider.minmax)
 		})
 
 		$(document).on('mousemove',(e)=>{
@@ -126,7 +180,8 @@ class controller{
 			(
 				this.handle.setPosition(this.slider.countMousePos(e)),
 				this.slider.minmaxCompute(),
-				this.slider.rangeElement?this.slider.rangeElement.render(...this.slider.minmax):null
+				this.slider.rangeElement?this.slider.rangeElement.render(...this.slider.minmax):null,
+				this.handle.title = this.slider.computeTitleValue(e) 
 			) : null
 		})
 		
