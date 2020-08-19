cb6a3473706ea55b0b2366f8c0ca67d9f246396c
tim <timon0596@yandex.ru>
2020-08-19 07:46:38 +0300

    string type maintenance

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import './plugin/plugin.ts'
 const c:any = []
 const opts:options[] = 
 [
-	{values: [100,600],step: 11,initialValues:[200,250,10],handles: 3},
+	{values: ['1','2','3','4','q','w','e','r','t'],step: 1,initialValues:[200,250,10],handles: 3},
 	{values: [100,600],step: 13,initialValues:[200,250,10],handles: 1, vertical:true},
 	{values: [0,160],step: 3,handles: 2},
 	{values: ['a','b','c','d'],handles: 1,step: 1},
--- src/model/model.ts
+++ src/model/model.ts
@@ -1,56 +1,60 @@
 export class Model{
-	private singleStep=0
-	private stepSize=0
-	private handlePosition=0
-	private sliderSize = 0
-	private type: string
-	private el:any = {}
-	private diapason = 0
-	constructor(private options:any){
-		this.type = typeof this.options.values[0]
-		this.diapason = this.type=='number'?(this.options.values[1] - this.options.values[0]):0
-	}
-	get handlePos(){
-		return this.handlePosition
-	}
-	computeSliderSize(slider:JQuery<HTMLElement>):void{
-		this.sliderSize = slider[0].getBoundingClientRect()[this.options.vertical?'height':'width']
-	}
-	computeSingleStep():void{
-		if(this.type=='string'){
-			this.singleStep = this.sliderSize/(this.options.values.length-1)
-		}
-		else{
-			this.singleStep =  this.sliderSize/(this.options.values[1]-this.options.values[0])
-		}
-	}
-	computeStepSize(){
-		this.stepSize = this.singleStep*this.options.step
-	}
-	getTitleValue(){
-		return Math.round(this.handlePosition/this.singleStep+this.options.values[0])
-	}
-	computeHandlePosition(e:JQuery.MouseMoveEvent|JQuery.ClickEvent|string|number,slider:JQuery<HTMLElement>):void{
-		if(typeof e=='string'){
+    private singleStep=0
+    private stepSize=0
+    private handlePosition=0
+    private sliderSize = 0
+    private type: string
+    private el:any = {}
+    private diapason = 0
+    constructor(private options:any){
+        this.type = typeof this.options.values[0]
+        this.diapason = this.type=='number'?(this.options.values[1] - this.options.values[0]):0
+    }
+    get handlePos(){
+        return this.handlePosition
+    }
+    computeSliderSize(slider:JQuery<HTMLElement>):void{
+        this.sliderSize = slider[0].getBoundingClientRect()[this.options.vertical?'height':'width']
+    }
+    computeSingleStep():void{
+        if(this.type=='string'){
+            this.singleStep = this.sliderSize/(this.options.values.length-1)
+        }
+        else{
+            this.singleStep =  this.sliderSize/(this.options.values[1]-this.options.values[0])
+        }
+    }
+    computeStepSize(){
+        this.stepSize = this.singleStep*this.options.step
+    }
+    getTitleValue(){
+        if(this.type=='number'){
+            return Math.round(this.handlePosition/this.singleStep+this.options.values[0])
+        }else{
+           return this.options.values[this.handlePosition/this.singleStep]
+        }
+    }
+    computeHandlePosition(e:JQuery.MouseMoveEvent|JQuery.ClickEvent|string|number,slider:JQuery<HTMLElement>):void{
+        if(typeof e=='string'){
 
-		}
-		else if(typeof e=='number'){
-			e=e-this.options.values[0]
-			this.handlePosition  = Math.round(e/this.diapason*this.sliderSize)
-			console.log(e/this.diapason)
-		}
-		else{
-			if(this.options.vertical){
-				this.handlePosition = 0
-			}
-			else{
-				const mousePosition =  e.pageX-slider[0].offsetLeft
-				const handleSteps = Math.round((mousePosition - this.handlePosition)/(e.type=='click'?this.singleStep:this.stepSize))
-				this.handlePosition+= handleSteps*(e.type=='click'?this.singleStep:this.stepSize)
-			}
-		}
-		this.handlePosition = this.handlePosition<0?0:this.handlePosition
-		this.handlePosition = this.handlePosition>this.sliderSize?this.sliderSize:this.handlePosition
-	}
-	
+        }
+        else if(typeof e=='number'){
+            e=e-this.options.values[0]
+            this.handlePosition  = Math.round(e/this.diapason*this.sliderSize)
+            console.log(e/this.diapason)
+        }
+        else{
+            if(this.options.vertical){
+                this.handlePosition = 0
+            }
+            else{
+                const mousePosition =  e.pageX-slider[0].offsetLeft
+                const handleSteps = Math.round((mousePosition - this.handlePosition)/(e.type=='click'?this.singleStep:this.stepSize))
+                this.handlePosition+= handleSteps*(e.type=='click'?this.singleStep:this.stepSize)
+            }
+        }
+        this.handlePosition = this.handlePosition<0?0:this.handlePosition
+        this.handlePosition = this.handlePosition>this.sliderSize?this.sliderSize:this.handlePosition
+    }
+    
 }
